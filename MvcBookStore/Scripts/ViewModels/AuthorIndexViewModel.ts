1c10e1d1ce918a7da03516a4297292d4abc8be93
Patrick Shih <patrick85081@gmail.com>
2018-10-21 20:47:01 +0800

    Delete Author Finish.

--- MvcBookStore/Scripts/ViewModels/AuthorIndexViewModel.ts
+++ MvcBookStore/Scripts/ViewModels/AuthorIndexViewModel.ts
@@ -1,5 +1,23 @@
 ﻿class AuthorIndexViewModel {
-    constructor(public authors: Author[]) {
+    sending = ko.observable(false);
 
+    constructor(public authors: Author[]) {
+    }
+
+    showDeleteModal = (data, event) => {
+        this.sending(false);
+
+        $.get($(event.target).attr('href'),
+            html => {
+                $('.body-content').prepend(html);
+                $('#deleteModal').modal('show');
+
+                ko.applyBindings(this, document.getElementById('deleteModal'));
+            });
+    }
+
+    deleteAuthor(form: HTMLFormElement) {
+        this.sending(true);
+        return true;
     }
 }
